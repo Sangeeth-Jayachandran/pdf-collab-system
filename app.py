99013d51fca02b1a8c2976cbf96e896e89ce9ecf
unknown <sangeetha.jayachandran123@gmail.com>
2025-05-31 11:55:06 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_mail import Mail
 from flask_bcrypt import Bcrypt
@@ -34,4 +35,4 @@ def create_application():
 application = create_application()
 
 if __name__ == '__main__':
-    application.run(debug=True)
+    application.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
